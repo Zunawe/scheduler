fc14544f77ec28ca98fde536e04324a3a04d6f1e
Zunawe <gyroscope15@gmail.com>
2023-01-28 17:46:21 -0800

    Add react-helmet and page title

--- client/js/index.tsx
+++ client/js/index.tsx
@@ -7,9 +7,13 @@ import ReactDOM from 'react-dom'
 
 import { App } from './App'
 import { ContextProvider } from './context'
+import { Helmet } from 'react-helmet'
 
 ReactDOM.render(
   <ContextProvider>
+    <Helmet>
+      <title>Scheduler</title>
+    </Helmet>
     <App />
   </ContextProvider>,
   document.getElementById('root')
